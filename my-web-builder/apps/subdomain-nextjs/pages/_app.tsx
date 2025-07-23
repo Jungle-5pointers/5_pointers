6deeb4f9e089345e4740e8cac11e6ac0a3f4cf46
Bell-isHere <quf417@gmail.com>
2025-07-23 16:43:05 +0900

    fix: 실제 모바일 기기 사이즈 대응

--- my-web-builder/apps/subdomain-nextjs/pages/_app.tsx
+++ my-web-builder/apps/subdomain-nextjs/pages/_app.tsx
@@ -14,10 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {/* 카카오 SDK - integrity 속성 완전 제거 */}
       <Script
